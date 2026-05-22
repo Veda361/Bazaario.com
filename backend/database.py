92bfeae02c5864fc63e0264022ba3e448d5bfdbe
Dev Sahu <devranjeetq@gmail.com>
2026-05-22 22:27:17 +0530

    Fix Supabase database connection

--- backend/database.py
+++ backend/database.py
@@ -2,8 +2,10 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from config import DATABASE_URL
 
-engine = create_engine(DATABASE_URL)
-
+create_engine(
+    DATABASE_URL,
+    connect_args={"sslmode": "require"}
+)
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
